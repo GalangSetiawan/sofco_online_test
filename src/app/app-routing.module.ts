0bc66f864eaa65dd7ae0ec73173365e63edb662d
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2021-01-13 10:37:27 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { CatalogComponent } from './component/catalog/catalog.component';
 import { ShoplistComponent } from './component/shoplist/shoplist.component';
+import { DetailbookComponent } from './component/detailbook/detailbook.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent }, 
-  { path: 'shoplist', component: ShoplistComponent }
+  { path: 'shoplist', component: ShoplistComponent },
+  { path: 'detail', component: DetailbookComponent }
+
 ]
 
 @NgModule({
