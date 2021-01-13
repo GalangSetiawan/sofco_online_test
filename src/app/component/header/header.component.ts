0c7cef4ce4f55cd91057cc14fa4de5e7d560abf4
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2021-01-13 10:40:51 +0700

    Update header.component.ts

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BookstoreService } from '../../service/bookstore.service'
+import { interval, Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  subscription: Subscription;
+  intervalId: number;
+  dataCart:any = [];
+  
+  constructor(private bookSvc:BookstoreService) {
+  }
+  
   ngOnInit() {
+    console.log('dataCart | shoplist===>',this.dataCart);
+    const source = interval(1000);
+    this.subscription = source.subscribe(val => this.dataCart = this.bookSvc.getDataCart());
   }
 
+ 
+  
+
 }
+
